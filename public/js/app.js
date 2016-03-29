f23fd87e2a8e9d6451b5422ed6b6237106368f08
Laura <lg3295@gmail.com>
2016-03-29 12:33:27 -1000

    adds bubblesort to Array prototype

--- public/js/app.js
+++ public/js/app.js
@@ -22,3 +22,27 @@ function bubbleSort(arr){
   }
   return count;
 };
+
+Array.prototype.bubbleSort = function(arr){
+  console.log('this',this);
+  var bubbledArr = arr;
+  var isSwapped = false;
+
+  while(!isSwapped) {
+    isSorted = false;
+    for(var i = 0; i < bubbledArr.length -1; i++){
+      var curr = bubbledArr[i];
+      var next = bubbledArr[i + 1];
+      var moves = 0;
+      if (curr > next) {
+        bubbledArr[i] = next;
+        bubbledArr[i + 1] = curr;
+        moves++
+      }
+    }
+    if (moves === 0){
+      isSwapped = true;
+    }
+  }
+  return bubbledArr;
+};
