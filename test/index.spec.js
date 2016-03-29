1f9b4408919ebb978f0d80e9ad771a38bfec81ff
Laura <lg3295@gmail.com>
2016-03-29 12:14:36 -1000

    tests that bubble sort function returns correct value

--- test/index.spec.js
+++ test/index.spec.js
@@ -14,7 +14,19 @@ describe('Bubble Sort', function(){
   });
 
   it('should return a number', function(){
-    expect(bubbleSort([3,2,1])).to.be.a('number');
+    var myArr = [3,2,1];
+    expect(bubbleSort(myArr)).to.be.a('number');
+  });
+
+  it('should return the necessary moves to sort', function(){
+    var myArr2 = [4,3,2,1];
+    expect(bubbleSort(myArr2)).to.equal(5);
+
+    var myArr3 = [5,1,4,7];
+    expect(bubbleSort(myArr3)).to.equal(2);
+
+    var myArr4 = [10,9,21,35,17];
+    expect(bubbleSort(myArr4)).to.equal(3);
   });
 
 });
