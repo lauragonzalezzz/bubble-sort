c99c57d78dbd75b4ef4d55de8ef24384287385ee
Laura <lg3295@gmail.com>
2016-03-29 17:00:53 -1000

    adds createArrayBars function, styles divs

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,23 @@
+var myArray = [4,3,2,1];
+
 var contentDiv = document.getElementById('content');
 var arrayOutput = document.createElement('div');
-arrayOutput.innerHTML = bubbleSort([5,4,3,2,1]);
+arrayOutput.innerHTML = bubbleSort(myArray);
 contentDiv.appendChild(arrayOutput);
+
+var sortButton = document.createElement('button');
+sortButton.innerHTML = 'Sort';
+contentDiv.appendChild(sortButton);
+
+var barsDiv = document.getElementById('barsDiv');
+function createArrayBars(arr){
+  for (var i = 0; i < arr.length; i++) {
+    var indvBar = document.createElement('div');
+    indvBar.classList.add('indvBar');
+    indvBar.innerHTML = arr[i];
+    barsDiv.appendChild(indvBar);
+
+  }
+};
+createArrayBars(myArray);
 
