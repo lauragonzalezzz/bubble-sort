15d35574ca5bd4ae7ba2857fe14b3ba93d9beb3b
Laura <lg3295@gmail.com>
2016-03-29 16:08:23 -1000

    modifys project to display in browser

--- public/js/bubble-sort.js
+++ public/js/bubble-sort.js
@@ -48,9 +48,3 @@ Array.prototype.bubbleSort = function(){
   }
   return this;
 };
-
-
-// var contentDiv = document.getElementById('content');
-// var arrayOutput = document.createElement('div');
-// arrayOutput.innerHTML = bubbleSort([5,4,3,2,1]);
-// contentDiv.appendChild(arrayOutput);
