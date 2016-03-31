910c283c9ce70325091f73b2b0df56fe62ed694f
Laura <lg3295@gmail.com>
2016-03-31 11:12:28 -1000

    removed npm test require links

--- public/js/tests/quick-sort.spec.js
+++ public/js/tests/quick-sort.spec.js
@@ -1,12 +1,3 @@
-var fs = require('fs');
-var vm = require('vm');
-var chai = require('chai');
-var functionsFile = fs.readFileSync(process.cwd() + '/public/js/quick-sort.js');
-vm.runInThisContext(functionsFile); // file runs and it's contents has access to GLOBAL
-
-var expect = chai.expect;
-var should = chai.should();
-
 describe('Quick Sort', function(){
 
   it('should be a function', function(){
