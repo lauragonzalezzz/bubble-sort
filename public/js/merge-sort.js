d0e5dec25d9fde808c834f063ce4f50abf817261
Laura <lg3295@gmail.com>
2016-03-31 12:26:00 -1000

    uses functions merge sort and arr merge to perform a merge sort on an array

--- public/js/merge-sort.js
+++ public/js/merge-sort.js
@@ -1,1 +1,61 @@
-function mergeSort(){};
+function mergeSort(arr){
+
+// Base Case
+  if (arr.length === 0){
+    return [];
+  }
+  if (arr.length === 1){
+    return arr;
+  };
+
+// Declare Variable Arrays
+  var leftArr = [];
+  var rightArr = [];
+
+// Iterate
+  for (var i = 0; i < arr.length; i++){
+    if (i % 2 === 0) {
+      leftArr.push(arr[i]);
+    }
+    else {
+      rightArr.push(arr[i]);
+    };
+  };
+// Recurse and Sort Left and Right Arrays
+  leftArr = mergeSort(leftArr);
+  rightArr = mergeSort(rightArr);
+
+// Run arrMerge Function on Left and Right Arrays, return
+  return arrMerge(leftArr, rightArr);
+
+}; //End of Merge Sort
+
+
+function arrMerge(left, right){
+  var result = [];
+console.log('left',left);
+console.log('right',right);
+  while (left.length > 0 && right.length > 0){
+    if (left[0] <= right[0]){
+      var lshifted = left.shift();
+      result.push(lshifted);
+    }
+    else {
+      var rshifted = right.shift();
+      result.push(rshifted);
+    };
+  };
+
+  while (left.length > 0){
+    var l2shifted = left.shift();
+    result.push(l2shifted);
+  };
+  while (right.length > 0){
+    var r2shifted = right.shift();
+    result.push(r2shifted);
+  };
+  console.log('result',result);
+  return result;
+
+};
+mergeSort([123,1,45,15,454,12,1,0]);
