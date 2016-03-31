30ecc67b3b0aa122fee16494aad2e060876bab6f
Laura <lg3295@gmail.com>
2016-03-31 12:27:23 -1000

    tests that mergeSort returns a sorted array

--- public/js/tests/merge-sort.spec.js
+++ public/js/tests/merge-sort.spec.js
@@ -8,7 +8,7 @@ describe('Merge Sort', function(){
     expect(mergeSort([3,2,4])).to.be.an('array');
   });
 
-  // it('should return a sorted array', function(){
-  //   expect(quickSort([5,4,3,67,3])).to.deep.equal([3,3,4,5,67]);
-  // });
+  it('should return a sorted array', function(){
+    expect(mergeSort([5,4,3,67,3])).to.deep.equal([3,3,4,5,67]);
+  });
 });
