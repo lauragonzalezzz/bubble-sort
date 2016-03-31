fb408b3589c9f3ec9c82e04a72f83ac344974752
Laura <lg3295@gmail.com>
2016-03-31 11:17:27 -1000

    tests that mergeSort is a function

---
+++ public/js/tests/merge-sort.spec.js
@@ -0,0 +1,14 @@
+describe('Merge Sort', function(){
+
+  it('should be a function', function(){
+    expect(mergeSort).to.be.a('function');
+  });
+
+  // it('should return an array', function(){
+  //   expect(quickSort([3,2,4])).to.be.an('array');
+  // });
+
+  // it('should return a sorted array', function(){
+  //   expect(quickSort([5,4,3,67,3])).to.deep.equal([3,3,4,5,67]);
+  // });
+});
