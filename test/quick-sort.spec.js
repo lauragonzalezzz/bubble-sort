9b66363b3d4d83fa3bfed7c2d88dd7f9a6abcbe8
Laura <lg3295@gmail.com>
2016-03-30 20:11:25 -1000

    tests that function returns an array

--- test/quick-sort.spec.js
+++ test/quick-sort.spec.js
@@ -14,10 +14,10 @@ describe('Quick Sort', function(){
   });
 
   it('should return an array', function(){
-    expect(quickSort()).to.be.an('array');
+    expect(quickSort([3,2,4])).to.be.an('array');
   });
 
-  // it('should return a sorted array', function(){
-  //   expect(quickSort()).to.be.an('array');
-  // });
+  it('should return a sorted array', function(){
+    expect(quickSort([5,4,3,67,3])).to.deep.equal([3,3,4,5,67]);
+  });
 });
