bdffda06fc2a867aadf869560c37296ccc12d412
Laura <lg3295@gmail.com>
2016-03-31 15:22:39 -1000

    removes unnecessary comments

--- public/js/selection-sort.js
+++ public/js/selection-sort.js
@@ -7,13 +7,11 @@ function selectionSort(arr){
 
   // comp is the index of the value the 'smallest' value is compared against
     for (var comp = i + 1; comp < arr.length; comp++){
-    // if comp is less than 'smallest', reassign smallest to the compared value
       if (arr[comp] < arr[smallest]) {
         smallest = comp;
       }; //Ends if statement 1
     }; //Ends for loop 2
 
-  // after a full loop, if the smallest number isn't the original smallest number,
   // switch the smallest number to the first index
     if (smallest !== i) {
       var limbo = arr[i];
