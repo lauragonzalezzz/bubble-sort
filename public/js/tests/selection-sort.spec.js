1b5ce8a4325d45b68e7303e19b7d9247aeb4d22b
Laura <lg3295@gmail.com>
2016-03-31 14:40:07 -1000

    tests that selection sort is a function

---
+++ public/js/tests/selection-sort.spec.js
@@ -0,0 +1,17 @@
+describe('Selection Sort', function(){
+
+  it('should be a function', function(){
+    expect(selectionSort).to.be.a('function');
+  });
+
+  // it('should return an array', function(){
+  //   expect(selectionSort([3,2,4])).to.be.an('array');
+  // });
+
+  // it('should return a sorted array', function(){
+  //   expect(selectionSort([5,4,3,67,3])).to.deep.equal([3,3,4,5,67]);
+  //   expect(selectionSort([1,2,3,4,5,6,7,8,9])).to.deep.equal([1,2,3,4,5,6,7,8,9]);
+  //   expect(selectionSort([654,0,123456,1])).to.deep.equal([0,1,654,123456]);
+  //   expect(selectionSort([0,-1,3])).to.deep.equal([-1,0,3]);
+  // });
+});
