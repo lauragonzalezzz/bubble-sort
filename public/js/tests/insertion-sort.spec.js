2836d980e93688107ad3fd799a83ee3a0dc4af8b
Laura <lg3295@gmail.com>
2016-03-31 14:29:57 -1000

    tests that insertionsort returns a sorted array

--- public/js/tests/insertion-sort.spec.js
+++ public/js/tests/insertion-sort.spec.js
@@ -4,11 +4,11 @@ describe('Insertion Sort', function(){
     expect(insertionSort).to.be.a('function');
   });
 
-  // it('should return an array', function(){
-  //   expect(insertionSort([3,2,4])).to.be.an('array');
-  // });
+  it('should return an array', function(){
+    expect(insertionSort([3,2,4])).to.be.an('array');
+  });
 
-  // it('should return a sorted array', function(){
-  //   expect(insertionSort([5,4,3,67,3])).to.deep.equal([3,3,4,5,67]);
-  // });
+  it('should return a sorted array', function(){
+    expect(insertionSort([5,4,3,67,3])).to.deep.equal([3,3,4,5,67]);
+  });
 });
