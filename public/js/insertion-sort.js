2352a95236035d03f8bf6ed45b0a2d890b132902
Laura <lg3295@gmail.com>
2016-03-31 14:29:33 -1000

    insertion sort complete, passes tests

--- public/js/insertion-sort.js
+++ public/js/insertion-sort.js
@@ -1,1 +1,14 @@
-function insertionSort(){};
+function insertionSort(arr){
+
+  for(var i = 1; i < arr.length; i++) {
+    curr = arr[i];
+    prev = i - 1;
+
+    while (prev >= 0 && arr[prev] > curr){
+      arr[prev+1] = arr[prev];
+      prev--;
+    };
+    arr[prev+1] = curr;
+  };
+  return arr;
+} //End of Insertion Sort
