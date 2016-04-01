a63f069322e42d4c058315eec4522b582c4fc15d
Laura <lg3295@gmail.com>
2016-03-31 14:32:21 -1000

    adds additional test to verify multiple array inputs sort properly

--- public/js/tests/insertion-sort.spec.js
+++ public/js/tests/insertion-sort.spec.js
@@ -10,5 +10,8 @@ describe('Insertion Sort', function(){
 
   it('should return a sorted array', function(){
     expect(insertionSort([5,4,3,67,3])).to.deep.equal([3,3,4,5,67]);
+    expect(insertionSort([1,2,3,4,5,6,7,8,9])).to.deep.equal([1,2,3,4,5,6,7,8,9]);
+    expect(insertionSort([654,0,123456,1])).to.deep.equal([0,1,654,123456]);
+    expect(insertionSort([0,-1,3])).to.deep.equal([-1,0,3]);
   });
 });
